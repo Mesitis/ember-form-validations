7bc5a0467cee7b0b4581179a4246519079f9cb48
Niranjan Rajendran <me@niranjan.io>
2019-06-25 13:11:06 +0530

    Run indicative through babel before packaging

--- .eslintrc.js
+++ .eslintrc.js
@@ -68,5 +68,39 @@ module.exports = {
   globals: {
     module: true,
     Promise: true
-  }
+  },
+  overrides: [
+    // node files
+    {
+      files: [
+        '.template-lintrc.js',
+        '.eslintrc.js',
+        'ember-cli-build.js',
+        'testem.js',
+        'blueprints/*/index.js',
+        'config/**/*.js',
+        'lib/*/index.js',
+        'server/**/*.js',
+        'index.js',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+        ecmaVersion: 2015
+      },
+      env: {
+        browser: false,
+        node: true
+      },
+      plugins: ['node'],
+      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
+        // add your custom rules and overrides for node files here
+
+        // this can be removed once the following is fixed
+        // https://github.com/mysticatea/eslint-plugin-node/issues/77
+        'node/no-unpublished-require': 'off',
+        'node/no-extraneous-require': 'off'
+      })
+    }
+  ],
+
 };
--- index.js
+++ index.js
@@ -1,8 +1,37 @@
 /* jshint node: true */
 'use strict';
+const path = require('path');
+
+let libraryToTranspile = 'indicative';
 
 module.exports = {
-  name: 'ember-form-validations',
+  name    : 'ember-form-validations',
+  options : {
+    autoImport: {
+      webpack: {
+        module: {
+          rules: [
+            {
+              test : new RegExp('^' + path.dirname(require.resolve(libraryToTranspile + '/package.json'))),
+              use  : {
+                loader  : 'ember-form-validations-babel-loader',
+                options : {
+                  presets: [
+                    ['@babel/preset-env', { targets: ['ie 11', 'last 2 versions'] }]
+                  ]
+                }
+              }
+            }
+          ]
+        },
+        resolveLoader: {
+          alias: {
+            'ember-form-validations-babel-loader': require.resolve('babel-loader')
+          }
+        }
+      }
+    }
+  },
   isDevelopingAddon() {
     return true;
   },
